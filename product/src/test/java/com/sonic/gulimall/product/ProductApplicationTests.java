438530082c934fad516815a5e56ea0e60576554d
ayumi64 <sonic64@qq.com>
2022-11-26 01:52:34 +0800

    first commit

---
+++ product/src/test/java/com/sonic/gulimall/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonic.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
